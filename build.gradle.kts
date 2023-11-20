a6776b0834381ebda27c163d01c37e03418c78a8
MinJunsu <sunsuking@gmail.com>
2023-11-20 18:37:01 +0900

    QueryDSL 설정 및 Security 설정 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     kotlin("jvm") version "1.8.22"
     kotlin("plugin.spring") version "1.8.22"
     kotlin("plugin.jpa") version "1.8.22"
+    kotlin("kapt") version "1.7.10"
 }
 
 group = "com.aim"
@@ -19,14 +20,39 @@ repositories {
     mavenCentral()
 }
 
+allOpen {
+    annotation("jakarta.persistence.Entity")
+    annotation("jakarta.persistence.MappedSuperclass")
+    annotation("jakarta.persistence.Embeddable")
+}
+
 dependencies {
+    // Kotlin Spring Libary
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    developmentOnly("org.springframework.boot:spring-boot-devtools")
+
+    // AIM Custom Libary
+    // Security
+    implementation("org.springframework.boot:spring-boot-starter-security")
+
+    // DB (JPA & QueryDSL)
     runtimeOnly("com.mysql:mysql-connector-j")
+    implementation("com.querydsl:querydsl-jpa:5.0.0:jakarta")
+    kapt("com.querydsl:querydsl-apt:5.0.0:jakarta")
+    kapt("jakarta.annotation:jakarta.annotation-api")
+    kapt("jakarta.persistence:jakarta.persistence-api")
+
+    // Dev Tools
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+
+
+    // Utils
+    implementation("io.github.oshai:kotlin-logging-jvm:5.1.0")
+    implementation("com.google.code.gson:gson:2.10.1")
+
+    // Test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
 }
