c665c68fd4a62fe1b0554a4e77a1c03a2bbadf65
MinJunsu <sunsuking@gmail.com>
2023-11-26 22:34:20 +0900

    IP 관련 어드민 속성 상수 추가

--- src/main/kotlin/com/aim/server/domain/admin/const/ConfigConsts.kt
+++ src/main/kotlin/com/aim/server/domain/admin/const/ConfigConsts.kt
@@ -5,6 +5,10 @@ class ConfigConsts {
         const val LOGIN_SESSION = "login_session"
         const val ADMIN_USERNAME_KEY = "admin_username"
         const val ADMIN_PASSWORD_KEY = "admin_password"
+        const val START_IP_ADDRESS_KEY = "start_ip_address"
+        const val END_IP_ADDRESS_KEY = "end_ip_address"
+        const val GATEWAY_IP_ADDRESS_KEY = "gateway_ip_address"
+        const val SUBNET_MASK_KEY = "subnet_mask"
         const val DEFAULT_ADMIN_USERNAME_VALUE = "admin"
         const val DEFAULT_ADMIN_PASSWORD_VALUE = "password123!"
     }
