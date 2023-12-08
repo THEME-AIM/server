c2199491841e34f235600fe80d3aa0a341e6c909
MinJunsu <sunsuking@gmail.com>
2023-12-08 13:13:44 +0900

    test properties 코드 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -73,6 +73,7 @@ dependencies {
     testImplementation("io.kotest:kotest-runner-junit5:5.5.5")
     testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
     testImplementation("io.mockk:mockk:1.13.4")
+    testImplementation("com.h2database:h2:1.4.200")
 }
 
 tasks.withType<KotlinCompile> {
