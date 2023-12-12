ff36093eb02b7e6b6ee5be2491fec590fd9258b9
MinJunsu <sunsuking@gmail.com>
2023-12-12 14:10:08 +0900

    관리자 데이터 로직 수정

--- src/main/kotlin/com/aim/server/domain/admin/repository/AdminConfigQueryRepositoryImpl.kt
+++ src/main/kotlin/com/aim/server/domain/admin/repository/AdminConfigQueryRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.aim.server.domain.admin.repository
 
+import com.aim.server.domain.admin.entity.AdminConfig
 import com.aim.server.domain.admin.entity.QAdminConfig.adminConfig
 import com.querydsl.jpa.impl.JPAQueryFactory
 import org.springframework.stereotype.Repository
@@ -17,5 +18,12 @@ class AdminConfigQueryRepositoryImpl(
                 .where(adminConfig.key.eq(key))
                 .fetchOne()
         )
+    }
+
+    override fun findAllAdminKeys(): List<AdminConfig> {
+        return queryFactory
+            .selectFrom(adminConfig)
+            .where(adminConfig.key.notIn("username", "password"))
+            .fetch()
     }
 }
