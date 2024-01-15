d235b707e2e3e2d65cfa65572301c8a0cc324937
mj.kim <alswo9853@gmail.com>
2024-01-15 13:57:05 +0900

    feat: Change logic -> delete origin make & new one

--- src/main/kotlin/com/aim/server/domain/address/service/OpenStackNetworkServiceImpl.kt
+++ src/main/kotlin/com/aim/server/domain/address/service/OpenStackNetworkServiceImpl.kt
@@ -153,15 +153,10 @@ class OpenStackNetworkServiceImpl(
         log.info { "ports: $ports" }
         val subnet = getSubnetList().first { it.id == port.fixedIps.first().subnetId }
         val subnetId = subnet.id
-        val networkId = subnet.networkId
         ports.remove(port)
         port.toBuilder().fixedIp(newIpAddress, subnetId).build()
-//        val newPort = Builders.port()
-//            .networkId(networkId)
-//            .fixedIp(newIpAddress, subnetId)
-//            .deviceId(serverId)
-//            .build()
         log.info { "newPort: $port" }
-        osAuthToken().networking().port().update(port)
+        osAuthToken().networking().port().delete(port.id)
+        osAuthToken().networking().port().create(port)
     }
 }
